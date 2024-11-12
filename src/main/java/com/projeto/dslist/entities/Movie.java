9d7af33234fae9af6eee3a916ca7f7833abbe7a3
pedro0402 <pedro693.ph@gmail.com>
2024-11-11 22:00:06 -0300

    Movie seed

--- src/main/java/com/projeto/dslist/entities/Movie.java
+++ src/main/java/com/projeto/dslist/entities/Movie.java
@@ -26,8 +26,10 @@ public class Movie {
 
     private String imgUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Movie(){
